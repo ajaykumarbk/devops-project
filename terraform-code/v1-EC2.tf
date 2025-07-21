8c5330b582d11511e3d4b893b4eceaab0e47b5b2
ajaykumarbk <ajaykumarbk34@gmail.com>
2025-07-21 04:40:18 +0000

    adding updated infra-code

--- terraform-code/v1-EC2.tf
+++ terraform-code/v1-EC2.tf
@@ -3,37 +3,99 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo_instance" {
-  ami           = "ami-020cba7c55df1f615" 
+  ami           = "ami-020cba7c55df1f615"
   instance_type = "t2.micro"
-  key_name = "demo"
-  security_groups = "demo-sg"
-
+  key_name = "data"
+  vpc_security_group_ids = [ aws_security_group.demo-sg.id]
+  subnet_id = aws_subnet.demo_public_subnet-01.id
+  associate_public_ip_address = true
   tags = {
     Name = "DemoInstance"
   }
-  
 }
 
 resource "aws_security_group" "demo-sg" {
-  name = "demo-sg"
-  description = "Security group for demo instance"
+  vpc_id = aws_vpc.demo.id
+  name        = "demo-sg"
+  description = "Allow SSH"
 
- ingress = {
-    description = "Allow SSH"
-    from_port = 22
-    to_port = 22
-    protocal = "tcp"
-    cidr_blocks = ["0.0.0.0.0/0"]
+  tags = {
+    Name = "allow_SSH"
+  }
+}
 
-    egress = {
-    description = "Allow all outbound traffic"
-    from_port = 0
-    to_port = 0   
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
- }
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id=aws_security_group.demo-sg.id
+  from_port        = 22
+  cidr_ipv4        = "0.0.0.0/0"
+  ip_protocol      = "tcp"
+  to_port          = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.demo-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc" "demo" {
+    cidr_block = "10.0.0.0/16"
+    
+    tags = {
+        Name = "DemoVPC"
+    }
   
 }
 
+resource "aws_subnet" "demo_public_subnet-01" {
+  vpc_id            = aws_vpc.demo.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+    
+    tags = {
+        Name = "DemoPublicSubnet-01"
+    }
+  
+}
+
+resource "aws_subnet" "demo_public_subnet-02" {
+  vpc_id            = aws_vpc.demo.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+    
+    tags = {
+        Name = "DemoPublicSubnet-02"
+    }
+  
+}
+
+resource "aws_internet_gateway" "demo-ig" {
+    vpc_id = aws_vpc.demo.id
+    
+    tags = {
+        Name = "DemoInternetGateway"
+    }
+  
+}
+
+resource "aws_route_table" "demo_public_rt" {
+  vpc_id = aws_vpc.demo.id
+  
+  route {
+    cidr_block="0.0.0.0/0"
+    gateway_id=aws_internet_gateway.demo-ig.id
+  }
+}
+
+resource "aws_route_table_association" "public-sunbnet-association-01" {
+  subnet_id = aws_subnet.demo_public_subnet-01.id
+  route_table_id = aws_route_table.demo_public_rt.id
+}
+
+resource "aws_route_table_association" "public-sunbnet-association-02" {
+  subnet_id = aws_subnet.demo_public_subnet-02.id
+  route_table_id = aws_route_table.demo_public_rt.id
 }
 
